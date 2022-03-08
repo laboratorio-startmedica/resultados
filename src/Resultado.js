18e0b5a69d9263509e50e0f7dae663c26ef2296e
en0c-026 <bitup.games@gmail.com>
2022-03-08 08:29:20 -0600

    arrex3

--- src/Resultado.js
+++ src/Resultado.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Resultado = ({id}) => {
   return (
-    <div>
-      <img src={`/result/${id}.jpg`} height="100%" />
+    <div height="100%">
+      <img src={`/resultados/result/${id}.jpg`} style={{ maxWidth: '100%', height: 'auto', width: 'auto' }} />
     </div>
   )
 }
